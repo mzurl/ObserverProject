137744edcea86be5ec0a7d4bb40982b343132cea
aiden ross <aiden.r.ross24@gmail.com>
2025-11-03 17:30:32 -0800

    update UiSubject to be abstract class

--- src/UiSubject.java
+++ src/UiSubject.java
@@ -1,3 +1,24 @@
-public interface UiSubject {
+import java.util.ArrayList;
+
+public abstract class UiSubject {
+    private ArrayList<UiObserver> observers;
+
+    protected UiSubject()
+    {
+        observers = new ArrayList<>();
+    }
     
+    public void register(UiObserver observer)
+    {
+    }
+    
+    public void unregister(UiObserver observer)
+    {
+    }
+
+    public abstract void notify(Channel ch);
+
+    public void toggleMode()
+    {
+    }
 }
